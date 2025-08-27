300200797b7046089341ff16c6ef35e728cabc49
Chris Hardee <chardee42@gmail.com>
2025-08-27 11:52:57 -0400

    Testing initial push

--- push_git_repo.bat
+++ push_git_repo.bat
@@ -1,3 +1,7 @@
-git remote add origin https://github.com/chardee42/customer-churn.git
-git branch -M main
-git push -u origin main
+rem git remote add origin https://github.com/chardee42/customer-churn.git
+rem git branch -M main
+rem git push -u origin main
+
+git add .
+git commit -m "Testing initial push"
+git push origin main
